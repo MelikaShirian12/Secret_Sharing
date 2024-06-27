c0ad347a05d9ed9d352b1d451cc14f6ec7528b9c
emika s <mel.shirian@gmail.com>
2024-06-27 15:17:49 +0330

    adding the secret class

---
+++ secret/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+
+
+
+
+
+
+class SecretFunc {
+
+    int s;
+    int t;
+    int n;
+    int p;
+    ArrayList<Integer> coefficients = new ArrayList<>();
+
+    Map<Integer, Integer> shareholder_secrets = new HashMap<>();
+}
